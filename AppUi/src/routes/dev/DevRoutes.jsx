fb828322ae34ff5676d6393f70591c7518938a84
ayush0316 <ayushkansal777@gmail.com>
2025-07-27 23:42:18 +0530

    Fix: corrected comman mistake in package.json and using import in DevRoutes.jsx

--- AppUi/src/routes/dev/DevRoutes.jsx
+++ AppUi/src/routes/dev/DevRoutes.jsx
@@ -1,6 +1,7 @@
 import { Route } from "react-router-dom";
 import ButtonHelp from "../../pages/help/ButtonHelp";
 import  FormComponentsHelp  from "../../pages/help/FormComponentsHelp"; 
+import ToastDemo from "../../pages/help/ToastDemo";
 
 const DevRoutes = () => {
   return (
