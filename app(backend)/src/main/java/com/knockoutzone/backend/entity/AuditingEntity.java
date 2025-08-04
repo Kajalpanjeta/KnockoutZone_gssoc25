93a34d9df266bc997d61ba86ba61ca111a1d76c7
KajalPanjeta <kajalpanjeta3@gmail.com>
2025-08-04 18:41:37 +0530

    Update Tournament, AuditingEntity, AuditorAwareImpl, and pom.xml for auditing setup

---
+++ app(backend)/src/main/java/com/knockoutzone/backend/entity/AuditingEntity.java
@@ -0,0 +1,38 @@
+package com.knockoutzone.backend.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Audited(withModifiedFlag = true)
+public class AuditingEntity {
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime UpdatedAt;
+
+    @CreatedBy
+    private String createBy;
+
+    @LastModifiedBy
+    private String updateBy;
+
+}
